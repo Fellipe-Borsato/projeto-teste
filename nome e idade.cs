021c7185c6c0fd568e2250d3513946594c55bd20
Fellipe-Borsato <fellipeborsato@gmail.com>
2024-03-01 16:43:16 -0300

    nome e idade
    agora tem idade tbm

---
+++ nome e idade.cs
@@ -0,0 +1,10 @@
+
+using System;
+class HelloWorld {
+  static void Main() {
+      string nome = "Fellipe" ;
+    int idade = int.Parse(Console.ReadLine());
+     Console.WriteLine($"{nome} {idade}");
+    
+  }
+}
